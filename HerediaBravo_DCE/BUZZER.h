af0caee163ded4f940a5a746bb81a49ee26961c6
Warabaringo <cris.herediabravo@alum.uca.es>
2025-05-05 13:19:24 +0200

    modificaciones

--- HerediaBravo_DCE/BUZZER.h
+++ HerediaBravo_DCE/BUZZER.h
@@ -2,25 +2,29 @@
  * BUZZER.h
  *
  * Created: 05/05/2025 10:02:00
- *  Author: DCE
+ *  Author: Cristian Heredia Bravo
  */ 
 
 
 #ifndef BUZZER_H_
 #define BUZZER_H_
 
+// Incluimos libreria
 #include <avr/io.h>
 
-#define BUZZER_0_mode DDRB
-#define BUZZER_0_in PINB
-#define BUZZER_0_out PORTB
+// Definimos para mayor comodidad
+#define BUZZER_0_mode DDRB // Modo entrada/salida
+#define BUZZER_0_in PINB // Para entrada
+#define BUZZER_0_out PORTB // Para salida
 
-#define BUZZER PORTB2
+#define BUZZER PORTB2 // Puerto donde está el BUZZER
 
 static inline void BUZZER_init() {
+	// Ponemos el BUZZER como salida
 	BUZZER_0_mode |= (1 << BUZZER);
 }
 
+// period_ms es el periodo (según esto suena más agudo o más grave) y n_times es cuantas veces se repite el sonido
 void BUZZER_playsound(uint8_t period_ms, uint8_t n_times);
 
 
--- HerediaBravo_DCE/main.c
+++ HerediaBravo_DCE/main.c
@@ -1,30 +1,39 @@
 /*
- * TEST_Buzzer.c
+ * main.c
  *
  * Created: 05/05/2025 10:01:08
- * Author : DCE
+ * Author : Cristian Heredia Bravo
  */ 
 
 
 #define F_CPU 16000000UL
-#define __DELAY_BACKWARD_COMPATIBLE__
+#define __DELAY_BACKWARD_COMPATIBLE__ // Para que delay sea compatible
+
+// Incluimos librerias del sistema
 #include <util/delay.h>
 #include <avr/io.h>
 
+// Incluimos librerias propias
 #include "../../DCE_libraries/MACROS.h"
 #include "BUZZER.h"
 
 int main(void)
 {
+	// Configuración inicial BUZZER
 	BUZZER_init();
-    /* Replace with your application code */
-    while (1) 
+
+	// <------------------------------> {} <------------------------------>
+    
+	while (1) 
     {
 		BUZZER_playsound(2, 10);
 		_delay_ms(100);
+
 		BUZZER_playsound(200, 10);
 		_delay_ms(100);
+		
 		BUZZER_playsound(100, 10);
+		_delay_ms(100);
 	}
 }
 
